7cc0fd066d2dcaee1b958e4ef510b35797d413f8
Paulo Tarud <paulo@sidekick.cl>
2021-02-23 12:23:18 -0300

    Add dependencies

--- lib/sbif_rails.rb
+++ lib/sbif_rails.rb
@@ -1,6 +1,12 @@
 require "sbif_rails/version"
+require 'sbif_rails/base'
+require 'sbif_rails/config'
+require 'sbif_rails/currency'
+require 'sbif_rails/dollar'
+require 'sbif_rails/euro'
+require 'sbif_rails/ipc'
+require 'sbif_rails/uf'
+require 'sbif_rails/utm'
 
 module SbifRails
-  class Error < StandardError; end
-  # Your code goes here...
 end
--- lib/sbif_rails/version.rb
+++ lib/sbif_rails/version.rb
@@ -1,3 +1,3 @@
 module SbifRails
-  VERSION = "0.1.0"
+  VERSION = "1.0.0"
 end
